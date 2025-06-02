1b988c8c1fd910f642a47f1d782c207ffc77a71c
Nikolai Lavrov <mail@nlavrov.ru>
2025-06-02 18:13:13 +0700

    v1.01 demo

--- src/app/parser/main.py
+++ src/app/parser/main.py
@@ -128,13 +128,3 @@ class Parser:
         )
         json_option.click()
         await asyncio.sleep(5)
-
-
-async def main():
-    parser = Parser()
-    url = "https://downloader.disk.yandex.ru/preview/aa4d906f50724e3c372ed50f6b3b362850a26bbd16e18442e5a5686005b8022d/683d9f41/TNHVeRgibI90AlqtnIefqAuSIq2TjNb65zb2WdH8S5oKYVpbhwxOHnDjKxzJzFJ8usTWMTb-5OmwLjQQ5QgxlA%3D%3D?uid=0&filename=photo_2025-06-02_12-44-28.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v3&size=2048x2048"
-    await parser.check(url)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
