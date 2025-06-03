4670401e38b472ff37250fbd5bf8fb33f73cafe9
Nikolai Lavrov <mail@nlavrov.ru>
2025-06-03 13:56:54 +0700

    v1.01 demo add model image

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ import asyncio
 from app.bot.main import start_bot
 
 
-async def main():
+def main():
     asyncio.run(start_bot())
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
