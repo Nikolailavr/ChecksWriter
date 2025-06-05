37cd075ca4c17694be0a09fa3786a66132cb789c
Nikolai Lavrov <mail@nlavrov.ru>
2025-06-05 14:56:55 +0700

    v1.02 demo Work version

--- src/app/celery/tasks.py
+++ src/app/celery/tasks.py
@@ -12,12 +12,14 @@ from celery.signals import task_success, task_failure
 log = logging.getLogger(__name__)
 
 
-@celery_app.task(bind=True)
+@celery_app.task
 def success_check(self, data: dict):
     asyncio.run(async_success_check(data))
 
 
 async def async_success_check(data: dict):
+    from app.bot.main import send_msg
+
     try:
         await ReceiptService.save_receipt(
             data=data["result"],
@@ -25,16 +27,12 @@ async def async_success_check(data: dict):
             category=data["category"],
         )
     except Exception as ex:
-        from app.bot.main import send_msg
-
         await send_msg(chat_id=data["chat_id"], text="❌ Ошибка, чек уже внесен")
     else:
-        from app.bot.main import send_msg
-
         await send_msg(chat_id=data["chat_id"], text="✅ Данные чека успешно внесены!")
 
 
-@celery_app.task(bind=True)
+@celery_app.task
 def failure_check(self, data: dict):
     asyncio.run(async_failure_check(data))
 
@@ -43,13 +41,6 @@ async def async_failure_check(data: dict):
     from app.bot.main import send_msg
 
     await send_msg(chat_id=data["chat_id"], text="❌ Ошибка, не удалось распознать!")
-
-
-def remove_file(data: dict):
-    try:
-        os.remove(os.path.abspath(settings.uploader.DIR / data["filename"]))
-    except FileNotFoundError as ex:
-        log.error(f"[ERROR] File not found {settings.uploader.DIR / data["filename"]}")
 
 
 @celery_app.task(bind=True)
@@ -72,28 +63,31 @@ def process_check(self, data: dict):
         remove_file(data)
 
 
+def remove_file(data: dict):
+    try:
+        os.remove(os.path.abspath(settings.uploader.DIR / data["filename"]))
+    except FileNotFoundError as ex:
+        log.error(f"[ERROR] File not found {settings.uploader.DIR / data["filename"]}")
+
+
 # Успешное выполнение задачи
 @task_success.connect
-def task_success_handler(
-    sender=None,
-    result=None,
-    **kwargs,
-):
+def task_success_handler(sender=None, result=None, **kwargs):
     log.info(f"✅ Задача '{sender.name}' выполнена успешно")
-    success_check.delay(result.get("data", dict))
+
+    # Проверка результата от process_check
+    if sender.name == "app.tasks.process_check" and isinstance(result, dict):
+        data = result.get("data")
+        if data:
+            success_check.delay(data)
 
 
 # Ошибка при выполнении задачи
 @task_failure.connect
 def task_failure_handler(
-    sender=None,
-    task_id=None,
-    exception=None,
-    args=None,
-    kwargs=None,
-    traceback=None,
-    einfo=None,
-    **other,
+    sender=None, task_id=None, exception=None, args=None, **kwargs
 ):
     log.error(f"❌ Задача '{sender.name}' завершилась с ошибкой: {exception}")
-    failure_check.delay(args[0])
+
+    if sender.name == "app.tasks.process_check" and args:
+        failure_check.delay(args[0])
