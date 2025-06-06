52ea077a506c4c57a28f9106e96de6f6ff177b69
Nikolai Lavrov <mail@nlavrov.ru>
2025-06-06 16:49:12 +0700

    v1.04 demo Work version

--- src/app/bot/handlers/main.py
+++ src/app/bot/handlers/main.py
@@ -4,6 +4,9 @@ from app.bot.handlers.other import register_other_handlers
 from app.bot.handlers.user import (
     register_users_commands_handlers,
     register_users_photos_handlers,
+    register_users_receipts_handlers,
+    register_users_categories_handlers,
+    register_users_other_handlers
 )
 
 
@@ -11,6 +14,9 @@ def register_all_handlers(dp: Dispatcher) -> None:
     handlers = (
         register_users_commands_handlers,
         register_users_photos_handlers,
+        register_users_receipts_handlers,
+        register_users_categories_handlers,
+        register_users_other_handlers,
         register_other_handlers,
     )
     for handler in handlers:
