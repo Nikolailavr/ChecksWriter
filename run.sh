f7ff7365f79342f17e7487731e70aed8a79f2455
Nikolai Lavrov <mail@nlavrov.ru>
2025-06-26 14:24:27 +0700

    v1.05 demo

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose down bot celery-1 flower
+docker-compose down
 docker volume prune -f
-docker-compose --env-file ./src/.env up -d --build bot celery-1 flower
+docker-compose --env-file ./src/.env up -d --build
 docker image prune -f
