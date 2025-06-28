da03d22628e6e429215960dcbe35cb29a5b821eb
Nikolai Lavrov <mail@nlavrov.ru>
2025-06-28 23:31:21 +0700

    v1.05 demo

--- run_db.sh
+++ run_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker volume prune -f
-docker-compose -f docker_compose.db.yml --env-file ./src/.env up -d --build
+docker-compose -f docker-compose.db.yml --env-file ./src/.env up -d --build
 docker image prune -f
