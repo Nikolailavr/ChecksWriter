df714598376fda60072c2a1e77c65eca638614b4
Nikolai Lavrov <mail@nlavrov.ru>
2025-08-06 16:00:46 +0700

    v1.06 demo

--- src/app/parser/main.py
+++ src/app/parser/main.py
@@ -107,7 +107,7 @@ class Parser:
             options.add_experimental_option("prefs", prefs)
 
             options.binary_location = "/usr/bin/chromium"
-            # options.add_argument("--headless=new")
+            options.add_argument("--headless=new")
             options.add_argument("--disable-gpu")
             options.add_argument("--window-size=1920,1080")
             options.add_argument("--no-sandbox")
