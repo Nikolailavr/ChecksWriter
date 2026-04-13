71e502b8c7addf5273bb152a398cf4b6ec22b419
lnv <mail@nlavrov.ru>
2026-04-13 14:25:10 +0700

    Update some bags

--- src/run_services.sh
+++ src/run_services.sh
@@ -4,4 +4,4 @@
 poetry run celery -A app.celery.celery_app worker --pool threads --loglevel=info &
 
 # Запуск flower
-poetry run celery -A app.celery.celery_app flower --port=5555
+#poetry run celery -A app.celery.celery_app flower --port=5555
